2910b5e6cd589cbd1df40e33f1863ec2b97300c9
Anthony <v81d@proton.me>
2025-10-08 11:02:16 -0700

    feat: add filters

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
+const filters = document.querySelector(".filters");
 const container = document.querySelector(".container");
+var selectedFilter = "All";
 
-async function getCards() {
-  const path = "data/cards.json";
-  const response = await fetch(path);
-  if (response.ok) {
-    console.log(await response.json());
-  }
-}
-
-const cards = getCards();
-
-const cardTemplate = (image, title, description, price) => {
+function fillCardTemplate(image, title, description, price) {
   return `
   <div class="card">
     <img class="card__image" src="${image}" alt="${title}" />
     <h2 class="card__title">${title}</h2>
     <h4 class="card__description">${description}</h4>
-    <button class="card__button">${price}</button>
+    <button class="card__button">$${price.toLocaleString("en-US")}</button>
   </div>
   `;
-};
-
-console.log(cards);
-
-for (const card of cards) {
-  container.insertAdjacentHTML("afterend", cardTemplate(card.image, card.title, card.description, card.price));
 }
+
+function loadCards(cards) {
+  for (const card of cards) {
+    container.insertAdjacentHTML(
+      "beforeend",
+      fillCardTemplate(card.image, card.title, card.description, card.price)
+    );
+  }
+}
+
+function selectFilter(filter) {}
+
+fetch("json/cards.json")
+  .then((response) => response.json())
+  .then((cards) => {
+    loadCards(cards);
+  });
