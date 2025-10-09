76136bc21fec7b743c96443ed0369cfe22ffc714
v81d <v81d@proton.me>
2025-10-08 22:30:22 -0400

    feat: complete filter selection

--- js/script.js
+++ js/script.js
@@ -8,13 +8,15 @@ function fillCardTemplate(image, title, description, price) {
     <img class="card__image" src="${image}" alt="${title}" />
     <h2 class="card__title">${title}</h2>
     <h4 class="card__description">${description}</h4>
-    <button class="card__button">$${price.toLocaleString("en-US")}</button>
+    <button class="card__button">$${price.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</button>
   </div>
   `;
 }
 
-function loadCards(cards) {
-  for (const card of cards) {
+function loadCards(cards, filterCategory) {
+  container.innerHTML = "";
+  const filteredCards = filterCategory === "All" ? cards : cards.filter((card) => card.category === filterCategory);
+  for (const card of filteredCards) {
     container.insertAdjacentHTML(
       "beforeend",
       fillCardTemplate(card.image, card.title, card.description, card.price)
@@ -22,10 +24,13 @@ function loadCards(cards) {
   }
 }
 
-function selectFilter(filter) {}
-
 fetch("json/cards.json")
   .then((response) => response.json())
   .then((cards) => {
-    loadCards(cards);
+    for (const input of filters.querySelectorAll("input[type='radio']")) {
+      input.addEventListener("change", () => {
+        selectedFilter = input.value;
+        loadCards(cards, selectedFilter);
+      });
+    }
   });
